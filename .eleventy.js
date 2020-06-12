544b5181f0c6f2a1a928197baa91a8a2e31b527a
Julio Rodriguez <juliorodriguez@ncl.com>
2020-06-12 16:19:52 -0400

    Added eleventy to project

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,18 @@
 const pluginSass = require("eleventy-plugin-sass");
 
+
 module.exports = function (eleventyConfig) {
     eleventyConfig
-        .addPassthroughCopy("./assets/js")
-        .addPassthroughCopy("./admin");
+        .addPassthroughCopy("./src/assets/js")
+        .addPassthroughCopy("./src/assets/")
+        .addPassthroughCopy("./src/admin")
+        .addLayoutAlias('default', 'layouts/default.njk');
     
     return {
         passthroughFileCopy: true,
         markdownTemplateEngine: "njk",
-        templateFormats: ["html", "md", "njk", "eot", "ttf", "woff", "woff2", "svg", "jpg", "png", "css", "svg", "yml", "js"],
         dir: {
+            templateFormats: ["html", "md", "njk", "eot", "ttf", "woff", "woff2", "svg", "jpg", "png", "css", "svg", "yml", "js"],
             input: "src",
             output: "_site",
             includes: "_includes"
