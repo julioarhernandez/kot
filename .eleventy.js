b2c20c9182d1b7847899fca460e9df5f67246714
Julio Rodriguez <juliorodriguez@ncl.com>
2020-10-05 11:45:23 -0400

    gallery image sizes updated

--- .eleventy.js
+++ .eleventy.js
@@ -13,16 +13,17 @@ module.exports = function (eleventyConfig) {
         let stats = await Image(src, {
           formats: [outputFormat],
           // This uses the original image width
-          widths: [350],
+          widths: [350, 650],
           urlPath: "/assets/img/optim/",
           outputDir: "_site/assets/img/optim/",
           // widths: [200] // output 200px maxwidth
           // widths: [200, null] // output 200px and original width
         });
 
-        let props = stats[outputFormat].pop();
+        let propssmall = stats[outputFormat][0];
+        let propslarge = stats[outputFormat][1];
 
-        return `<img src="${props.url}" width="${props.width}" height="${props.height}" alt="${alt}">`;
+        return `<a data-fancybox="gallery" href="${propslarge.url}"><img src="${propssmall.url}" width="${propssmall.width}" height="${propssmall.height}" alt="${alt}"></a>`;
       });
 
     eleventyConfig.addNunjucksFilter("addDotAtStart", function(value) {
